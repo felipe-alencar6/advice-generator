72bc5810790a00a0ea2add045be961ec5d5a23e9
Felipe Alencar <gia.comini@hotmail.com>
2022-09-02 15:11:22 -0300

    sending files to git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'advice-generator';
+  data = '';
+  constructor() {}
+  x = 0;
+  getAdvice() {
+    this.x = Math.floor(Math.random() * 220);
+    fetch('https://api.adviceslip.com/advice/' + this.x)
+      .then((response) => response.json())
+      .then((data) => {
+        this.data = data.slip.advice;
+        console.log(this.data);
+      });
+  }
+
+  ngOnInit(): void {
+    this.x = Math.floor(Math.random() * 220);
+    fetch('https://api.adviceslip.com/advice/' + this.x)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.data = data.slip.advice;
+        console.log(this.data);
+      });
+  }
 }
